fba1ce50b863fa2d811838c50bca2fb103730bb6
edumeneses <36012126+edumeneses@users.noreply.github.com>
2025-12-30 14:04:26 +0000

    Deploying to gh-pages from @ edumeneses/edumeneses.github.io@5c2063404f9a09b9ff356548161303a6d14ac64a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,12 +409,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
+          section: "News",},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
@@ -454,6 +449,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-puara",
+          title: 'Puara',
+          description: "Puara is a framework for building and deploy new media installations and New Interfaces for Music Expression",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/Puara/";
             },},{
         id: 'social-academia_edu',
         title: 'Academia_edu',
