6117f504413734b04f04feb022be16ed90378476
edumeneses <36012126+edumeneses@users.noreply.github.com>
2025-12-30 00:30:07 +0000

    Deploying to gh-pages from @ edumeneses/edumeneses.github.io@b0caa36d9944d8cfb1f6179b5ca06fe439847c1a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -462,13 +462,6 @@ ninja.data = [{
           window.open("", "_blank");
         },
       },{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
